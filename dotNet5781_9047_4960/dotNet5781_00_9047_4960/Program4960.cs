b220116d754f8c7ea2ae1ad26d3a3bf3332a2c89
AlboOdelia <alboodelia@gmail.com>
2020-10-22 18:41:02 +0300

    intial code+ welcome9047 update program

---
+++ dotNet5781_9047_4960/dotNet5781_00_9047_4960/Program4960.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_9047_4960
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome4960();
+            Console.ReadKey();
+        }
+
+        private static void welcome4960()
+        {
+            Console.WriteLine("Enter your name: ");
+            string myString = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", myString);
+        }
+        static partial void welcome9047();
+    }
+}
