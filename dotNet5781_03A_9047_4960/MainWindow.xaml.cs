7d7304c68491e6fe92f84a4ebe6adfd2278e91d0
raaya grinfeld <rgrinfel@g.jct.ac.il>
2020-11-22 17:18:50 +0200

    hhh

--- dotNet5781_03A_9047_4960/MainWindow.xaml.cs
+++ dotNet5781_03A_9047_4960/MainWindow.xaml.cs
@@ -39,7 +39,8 @@ namespace dotNet5781_03A_9047_4960
             cbBusLines.DisplayMemberPath = " BusLineNum ";
             cbBusLines.SelectedIndex = 0;
             ShowBusLine();
-            private dotNet5781_02_9047_4960.Program.BusLine currentDisplayBusLine;
+        }
+           private dotNet5781_02_9047_4960.Program.BusLine currentDisplayBusLine;
         private void ShowBusLine(int index)
         {
             currentDisplayBusLine = busLineCollection[index];
