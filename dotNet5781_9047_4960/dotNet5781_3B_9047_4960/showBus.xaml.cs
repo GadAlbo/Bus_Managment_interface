1bb1c0219d30861f3fb84213752aac3726afec4b
AlboOdelia <alboodelia@gmail.com>
2020-12-13 00:34:17 +0200

    jo

--- dotNet5781_9047_4960/dotNet5781_3B_9047_4960/showBus.xaml.cs
+++ dotNet5781_9047_4960/dotNet5781_3B_9047_4960/showBus.xaml.cs
@@ -54,6 +54,7 @@ namespace dotNet5781_3B_9047_4960
         {
             (e.Argument as Bus).State = state.refueling;
             Thread.Sleep(12000);
+            int i = 0;
         }
         private void Worker_RunWorkerCompletedRefuel(object sender, RunWorkerCompletedEventArgs e)
         {
