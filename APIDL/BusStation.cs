309bf621d243157b1e330b65799c9528672f38f8
raaya grinfeld <rgrinfel@g.jct.ac.il>
2020-12-22 14:59:31 +0200

    j

---
+++ APIDL/BusStation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace DO
+{
+    class BusStation
+    {
+        public int BusStationKey { get; set; }
+        public GeoCoordinate Coordinates { get; set; }
+        public string StationName { get; set; }
+        public string StationAddress{ get; set; }
+        public bool HasARoof { get; set; } //we need to add more options
+        public bool IsActive { get; set; }
+
+    }
+}
