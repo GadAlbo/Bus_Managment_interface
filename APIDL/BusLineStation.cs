1e5ffd7ec1b732a4c8175385415c4035c84c398f
raaya grinfeld <rgrinfel@g.jct.ac.il>
2020-12-23 16:24:03 +0200

    i

---
+++ APIDL/BusLineStation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    public class BusLineStation
+    {
+        public int BusLineStationKey { get; set; }
+        public int BusLineStationCode { get; set; }// i think it is need to be the key of the bus station
+        public int StationNumberInLine { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
