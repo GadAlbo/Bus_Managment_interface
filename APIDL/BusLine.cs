34d0ab48e28a469b59400318363f61e438b16319
AlboOdelia <alboodelia@gmail.com>
2020-12-23 16:25:34 +0200

    M

--- APIDL/BusLine.cs
+++ APIDL/BusLine.cs
@@ -5,13 +5,14 @@ using System.Device.Location;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DO
 {
     public class BusLine
     {
         public int BusLineKey { get; set; }
         public int LineNumber { get; set; }
-        public string Area { get; set; }
+        public Areas Area { get; set; }
         public int FirstStation { get; set; }
         public int LastStation { get; set; }
         public bool IsActive { get; set; }
--- APIDL/Enums.cs
+++ APIDL/Enums.cs
@@ -6,9 +6,6 @@ using System.Threading.Tasks;
 
 namespace DO
 {
-    class Enums
-    {
-        public enum Areas { General, North, South, Center, Jerusalem };// the opsional areas
-        public enum state { ReadyToGo, midRide, refueling, handling };
-    }
+    public enum Areas { General, North, South, Center, Jerusalem };// the opsional areas
+    public enum state { ReadyToGo, midRide, refueling, handling };
 }
