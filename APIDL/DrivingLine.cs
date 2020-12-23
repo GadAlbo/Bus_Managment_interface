df87a62860220de7e51aa867938628f0db716011
AlboOdelia <alboodelia@gmail.com>
2020-12-23 15:05:10 +0200

    MO

--- APIDL/DrivingLine.cs
+++ APIDL/DrivingLine.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace DO
 {
-    public class DrivingBus
+    public class DrivingLine
     {
        // public int BusKey { get; set; }
-        public string LicenseNumber { get; set; }
+        //public string LicenseNumber { get; set; }
         public int LineNumber { get; set; }
         // public int LineKey { get; set; } it in // because i dont anderstend why you need it
         public DateTime Start { get; set; }
--- APIDL/IDAL.cs
+++ APIDL/IDAL.cs
@@ -7,15 +7,38 @@ using System.Threading.Tasks;
 using DO;
 
 namespace APIDL
-
 {
     public interface IDAL
     {
+        #region Bus //i think we need to erace it
         bool addBus(Bus bus);
         bool update(Bus bus);
         void delete(Bus bus);
         Bus read(int license);
-        bool addBusInTravel(DrivingBus bus);
+        #endregion
+
+        #region BusLine
+        IEnumerable<BusLine> GetAllBusLines();
+        IEnumerable<BusLine> GetAllBusLinesBy(Predicate<BusLine> predicate);
+        bool AddBusLine(BusLine bus);
+        bool UpdateBusLine(BusLine bus);
+        bool UpdateBusLine(int busLineKey, Action<BusLine> update); //method that knows to updt specific fields in Person
+        void DeleteBusLine(BusLine bus);
+        BusLine GetBusLine(int busLineKey);
+        #endregion
+
+        #region DrivingLine
+        bool addLineInTravel(DrivingLine bus);
+        IEnumerable<DrivingLine> GetBusLinetInDriveList(Predicate<DrivingLine> predicate);
+        #endregion
+
+        #region BusStation
+        BusStation GetBusStation(int busStationKey);
+        #endregion
+
+        #region User
+        User GetUser(string userName);
+        #endregion
 
     }
 }
