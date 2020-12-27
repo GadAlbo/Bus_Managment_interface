d365fe01a58833e6f74a47746ed04cbe9de11f3e
raaya grinfeld <rgrinfel@g.jct.ac.il>
2020-12-27 16:47:02 +0200

    yy

---
+++ BL/Station.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+namespace BO
+{
+    public class Station
+    {
+        public int Key { get; set; }
+       // public BusStation BusStation { get; set; }
+        // public BusLineStation BusLineStation { get; set; }
+
+
+    }
+}
