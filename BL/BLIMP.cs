c229fd7c61fa50b4695e0c1d5d905213b400a3cd
raaya grinfeld <rgrinfel@g.jct.ac.il>
2020-12-30 16:08:57 +0200

    h

--- BL/BLIMP.cs
+++ BL/BLIMP.cs
@@ -9,13 +9,46 @@ namespace BL
 {
     public class BlImp1 : IBL
     {
-        #region Line
+        public void AddDeatinationStation(int stationKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSourceStation(int stationKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddStation(int stationKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleatStation(int stationKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime DistanceBetweanStations(int firstStationKey, int lastStationKey)
+        {
+            throw new NotImplementedException();
+        }
+        #region BusLineBO
+
+        BO.BusLineBO BusLineDOBOAdapter(DO.BusLine busLineDo)
+        {
+            BO.BusLineBO busLineBO = new BO.BusLineBO();
+            busLineDo.CopyPropertiesTo(busLineBO);
+            return busLineBO;
+        }
         public bool HasBusStation(int stationKey)
         {
-            BusLineStation lineStation = new BusLineStation();
-            lineStation.BusLineStationKey = stationKey;
-
             return true;
+        }
+
+        public bool HasLine(int lineNumber)
+        {
+            throw new NotImplementedException();
         }
         #endregion
     }
